826a9578f90f10c3fcb6a57fb3048f66b4062e85
poom <panuwich@mfec.co.th>
2020-01-15 13:08:22 +0700

    Revert "add project"
    This reverts commit a58d2e258ff9422f96c0eb75534ec5ea41fe4e9d.

--- src/main/java/com/example/cruddemo/repository/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.cruddemo.repository;
-
-import com.example.cruddemo.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Integer> {
-    
-}
--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES (1,'Ticket-01','Blackpink concert ticket',79000.00,1);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES (2,'Ticket-02','MVC ticket',3000.00,200);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES(3,'Ticket-03','Avenger: Endgame ticket',1500.00,68);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES(4,'Project-A','Bridge bar voucher',10000.00,100);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES(5,'Project-F','Brain junior',50.00,650);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES(6,'10062','Member A',899.00,2);
-INSERT INTO product (productno, productid, productname, productprice, productqty) VALUES(7,'10063','Member B',699.00,15);
