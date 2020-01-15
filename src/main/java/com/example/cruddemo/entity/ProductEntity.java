8ad28c8c8cd9f9c4e8c0aef978dac3998a454999
poom <panuwich@mfec.co.th>
2020-01-15 18:03:35 +0700

    add lombok to project

--- src/main/java/com/example/cruddemo/entity/ProductEntity.java
+++ src/main/java/com/example/cruddemo/entity/ProductEntity.java
@@ -1,10 +1,13 @@
 package com.example.cruddemo.entity;
 
+import lombok.Data;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
+@Data
 @Table(name = "product")
 public class ProductEntity {
     @Id
@@ -13,59 +16,4 @@ public class ProductEntity {
     private String productname;
     private Double productprice;
     private Integer productqty;
-
-    public ProductEntity() {
-
-    }
-
-    public Integer getProductno() {
-        return productno;
-    }
-
-    public void setProductno(Integer productno) {
-        this.productno = productno;
-    }
-
-    public String getProductid() {
-        return productid;
-    }
-
-    public void setProductid(String productid) {
-        this.productid = productid;
-    }
-
-    public String getProductname() {
-        return productname;
-    }
-
-    public void setProductname(String productname) {
-        this.productname = productname;
-    }
-
-    public Double getProductprice() {
-        return productprice;
-    }
-
-    public void setProductprice(Double productprice) {
-        this.productprice = productprice;
-    }
-
-    public Integer getProductqty() {
-        return productqty;
-    }
-
-    public void setProductqty(Integer productqty) {
-        this.productqty = productqty;
-    }
-
-    @Override
-    public String toString() {
-        return "ProductEntity{" +
-                "productno=" + productno +
-                ", productid='" + productid + '\'' +
-                ", productname='" + productname + '\'' +
-                ", productprice=" + productprice +
-                ", productqty=" + productqty +
-                '}';
-    }
 }
